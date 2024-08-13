ac622a7e1f3e71347675ca36763a1148f6deea23
James <jhmorley@icloud.com>
2024-08-13 21:51:24 +0100

    Install react-bootstrap
    Normal bootstrap commands also work examples of both using buttons present

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel'
 import './App.css'
 
 function App() {
@@ -6,6 +7,10 @@ function App() {
   return (
     <>
     <h1>Hi</h1>
+    <button type="button" className="btn btn-primary">Primary</button>
+    
+    <Button variant="success">Success</Button>{' '}
+
     </>
   )
 }

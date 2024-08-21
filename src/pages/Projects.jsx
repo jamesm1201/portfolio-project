446cb2d50a415d9fa44900c43b4d8d624341f541
James <jhmorley@icloud.com>
2024-08-21 19:15:00 +0100

    Prepping for content

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,14 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Projects(){
     return(
         <>
         <Header />
         <div>
-            <h1>This is the projects page</h1>
+            <h1>This is the projects page</h1> 
         </div>
-        {/* <Footer /> */}
+        <Footer />
         </>
     );
 }

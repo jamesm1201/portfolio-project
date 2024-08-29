016204e06e82a5724dc072063708a70426cce541
James <jhmorley@icloud.com>
2024-08-29 18:19:38 +0100

    footer is ruined cant figure out height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,15 @@ import * as Bs from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import githubLogo from "../assets/github-mark-white.png";
 import jmLogo from "../assets/jm-no-back.png";
-import linkedinWhite from '../assets/linkedin-white.png'
-import './footer.css'
+import linkedinWhite from "../assets/linkedin-white.png";
+import "./footer.css";
 
 export default function Footer() {
   return (
-      <Bs.Container fluid className="bg-dark footer" >
-        <Bs.Row >
-          <Bs.Col xl="3" className="mb-4 mt-4">
+      <footer fluid className="bg-dark h-auto">
+      {/* // <Bs.Container fluid className="bg-dark"> */}
+        <Bs.Row xl="3">
+          <Bs.Col xl="3" className="mb-4 mt-4 ">
             <img src={jmLogo}></img>
           </Bs.Col>
           <Bs.Col xl="3"></Bs.Col>
@@ -18,18 +19,24 @@ export default function Footer() {
               <h3>Links</h3>
             </Bs.Row>
             <Bs.Row>
-              <Bs.Button href="https://github.com/jamesm1201" className='w-auto mx-auto'>
+              <Bs.Button
+                href="https://github.com/jamesm1201"
+                className="w-auto mx-auto"
+              >
                 <img src={githubLogo}></img>
               </Bs.Button>
             </Bs.Row>
             <Bs.Row>
-              <Bs.Button href="https://www.linkedin.com/in/james-morley-736b79209" className='w-auto mx-auto'>
+              <Bs.Button
+                href="https://www.linkedin.com/in/james-morley-736b79209"
+                className="w-auto mx-auto"
+              >
                 <img src={linkedinWhite}></img>
               </Bs.Button>
             </Bs.Row>
           </Bs.Col>
         </Bs.Row>
-      </Bs.Container>
-    
+      {/* // </Bs.Container> */}
+  </footer>
   );
 }

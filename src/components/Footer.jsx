75c04125d11cf72eacb8fcdab0a52fe6b219fbc7
James <jhmorley@icloud.com>
2024-09-04 17:04:32 +0100

    structure footer with links
    container element in footer element for proper nesting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,34 +7,34 @@ import "./footer.css";
 
 export default function Footer() {
   return (
-      <footer fluid className="bg-dark h-auto">
-        <Bs.Row xl="3">
-          <Bs.Col xl="3" className="mb-4 mt-4 ">
-            <img src={jmLogo}></img>
+    <footer className="bg-dark">
+      <Bs.Container fluid>
+        <Bs.Row>
+          <Bs.Col xl="3" className="mb-5 mt-5 ms-5 me-5 align-items-center">
+            <img src={jmLogo} width={120} height={120} />
+            <p className="text-light mt-3">James Morley</p>
           </Bs.Col>
-          <Bs.Col xl="3"></Bs.Col>
-          <Bs.Col xl="6">
-            <Bs.Row>
-              <h3>Links</h3>
-            </Bs.Row>
-            <Bs.Row>
-              <Bs.Button
-                href="https://github.com/jamesm1201"
-                className="w-auto mx-auto"
-              >
-                <img src={githubLogo}></img>
-              </Bs.Button>
-            </Bs.Row>
-            <Bs.Row>
-              <Bs.Button
-                href="https://www.linkedin.com/in/james-morley-736b79209"
-                className="w-auto mx-auto"
-              >
-                <img src={linkedinWhite}></img>
-              </Bs.Button>
-            </Bs.Row>
+          <Bs.Col className="p-5">
+            
+          </Bs.Col>
+          <Bs.Col className="p-5">
+            <h3 className="text-light">Get in touch!</h3>
+            <p className="text-light">jhmorley@icloud.com</p>
+            <Bs.Button
+              href="https://github.com/jamesm1201"
+              className=" btn-dark"
+            >
+              <img src={githubLogo} width={50} height={50} />
+            </Bs.Button>
+            <Bs.Button
+              href="https://www.linkedin.com/in/james-morley-736b79209"
+              className="btn-dark"
+            >
+              <img src={linkedinWhite} width={50} height={50} />
+            </Bs.Button>
           </Bs.Col>
         </Bs.Row>
-  </footer>
+      </Bs.Container>
+    </footer>
   );
 }
